42fcac04032aa7ceaca00eee9a719459d9ea030d
muhammedshazinsameer <muhammedshazinsameer@gmail.com>
2025-11-17 22:09:22 +0530

    Update ordercontroller.js

--- backend/controllers/ordercontroller.js
+++ backend/controllers/ordercontroller.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req,res)=>{
 
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://superb-queijadas-ad430e.netlify.app";
 
     try {
         const newOrder = new OrderModel({
