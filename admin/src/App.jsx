8d84c96892345c875f33d0c93b66d832939cf577
shazin <muhammedshazinsameer@gmail.com>
2025-11-17 21:30:27 +0530

    netlify commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url ="http://localhost:4000"
+  const url ="https://food-del-mern-r8jg.onrender.com"
   return (
     <div>
       <ToastContainer/>
